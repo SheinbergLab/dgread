c501f69ae70624b8136fcc50ff138803472fa000
David Sheinberg <David_Sheinberg@brown.edu>
2023-04-07 15:34:33 -0400

    added zlib

--- python/dgread/setup.py
+++ python/dgread/setup.py
@@ -5,6 +5,6 @@ from distutils.extension import Extension
 import numpy as np                           # <---- New line
 
 MOD = 'dgread'
-sources=['dgread.c','df.c','dfutils.c','dynio.c','flip.c','lz4utils.c']
+sources=['dgread.c','df.c','dfutils.c','dynio.c','flip.c','lz4utils.c', 'lz4frame.c', 'lz4hc.c', 'lz4.c', 'xxhash.c']
 setup(name=MOD, version='1.01',ext_modules=[
-        Extension(MOD, sources=sources, include_dirs=['./',np.get_include()], library_dirs=['/usr/local/lib'])])
+        Extension(MOD, sources=sources, include_dirs=['./',np.get_include()], library_dirs=['/usr/local/lib'], libraries=['z'])])
