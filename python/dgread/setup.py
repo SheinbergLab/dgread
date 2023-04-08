c0bfca6e4b346d34bac41a2bdd2fe2c3606dc96c
David Sheinberg <David_Sheinberg@brown.edu>
2023-04-08 12:11:11 -0400

    include local folder for libs (amd64)

--- python/dgread/setup.py
+++ python/dgread/setup.py
@@ -6,5 +6,5 @@ import numpy as np                           # <---- New line
 
 MOD = 'dgread'
 sources=['dgread.c','df.c','dfutils.c','dynio.c','flip.c','lz4utils.c', 'lz4frame.c', 'lz4hc.c', 'lz4.c', 'xxhash.c']
-setup(name=MOD, version='1.01',ext_modules=[
-        Extension(MOD, sources=sources, include_dirs=['./',np.get_include()], library_dirs=['/usr/local/lib'], libraries=['z'])])
+setup(name=MOD, version='1.0.1',ext_modules=[
+        Extension(MOD, sources=sources, include_dirs=['./',np.get_include()], library_dirs=['/usr/local/lib', '.'], libraries=['z'])])
