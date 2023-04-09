61a7e867faa1475f5071637107de310bb4150ab1
David Sheinberg <David_Sheinberg@brown.edu>
2023-04-09 11:46:24 -0400

    added Windows library path

--- python/dgread/setup.py
+++ python/dgread/setup.py
@@ -2,9 +2,16 @@
 
 from distutils.core import setup
 from distutils.extension import Extension
-import numpy as np                           # <---- New line
+import platform
+import numpy as np                          
+version = "1.0.2"
 
 MOD = 'dgread'
 sources=['dgread.c','df.c','dfutils.c','dynio.c','flip.c','lz4utils.c', 'lz4frame.c', 'lz4hc.c', 'lz4.c', 'xxhash.c']
-setup(name=MOD, version='1.0.1',ext_modules=[
-        Extension(MOD, sources=sources, include_dirs=['./',np.get_include()], library_dirs=['/usr/local/lib', '.'], libraries=['z'])])
+library_dirs = ['/usr/local/lib']
+
+if platform.system() == 'Windows':
+    library_dirs.append('.')
+
+setup(name=MOD, version=version ,ext_modules=[
+        Extension(MOD, sources=sources, include_dirs=['./',np.get_include()], library_dirs=library_dirs, libraries=['z'])])
